1eb10e897cb367b8704dfd648105fbe30b1eaf65
Katerina Miroslavskaya <92998292+KetiMiros@users.noreply.github.com>
2024-02-26 03:47:21 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,43 @@
+package toyShop;
+
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ToyConstructor toy1 = new ToyConstructor(0, (float) 1.5, "constructor");
+        ToyConstructor toy2 = new ToyConstructor(1, (float) 2.5, "robot");
+        ToyConstructor toy3 = new ToyConstructor(2, (float) 6.0, "car");
+
+        ArrayList<ToyConstructor> assortiment = new ArrayList<>();
+        assortiment.add(toy1);
+        assortiment.add(toy2);
+        assortiment.add(toy3);
+
+        ToyMachine shop = new ToyMachine(3);
+        shop.put(toy1.toString(), toy2.toString(), toy3.toString());
+
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println(shop.get());
+        // }
+        FileWorking fw = new FileWorking();
+        String assortInfo = "Ваши вероятные варианты выйгрыши: " + assortiment;
+        fw.save(assortInfo);
+        fw.save("\n");
+        System.out.println(assortInfo);
+
+        for (int i = 0; i < 10; i++) {
+            shop.pollInQueue(assortiment);
+        }
+
+        String shopBuffer = "В Магазине Игрушек в данный момент находятся следующие игрушки: " + shop.pq;
+        fw.save("\n");
+        fw.save(shopBuffer);
+        fw.save("\n");
+        System.out.println(shopBuffer);
+
+        for (int i = 0; i < 10; i++) {
+            shop.takeFromQueue();
+        }
+    }
+}
